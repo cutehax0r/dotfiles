6d934500b776098c4354b7e28887c3e854de2bb8
cutehaX0r <97819453+cutehax0r@users.noreply.github.com>
2026-01-03 15:29:52 -0700

    Drop the treesitter augroup and rely on treesitter-modules
    This looks like it was a bit redundant. Should have equivalent
    functionality without the need to explicitly exclude types as
    treesitter-modules /should/ be only enabling TS for things that we have
    parsers installed for.

--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,34 +1,3 @@
--- auto install treesitter languages and enable folding / indenting
-vim.api.nvim_create_autocmd({ "Filetype" }, {
-  callback = function(event)
-    -- don't run on things you aren't going to care about the ast in.
-    local ignored_fts = {
-      "checkhealth",
-      "snacks_dashboard",
-      "snacks_notif",
-      "snacks_input",
-      "prompt",
-    }
-    if vim.tbl_contains(ignored_fts, event.match) then return end
-
-    local ok, nvim_treesitter = pcall(require, "nvim-treesitter")
-    if not ok then return end
-
-    local ft = vim.bo[event.buf].ft
-    local lang = vim.treesitter.language.get_lang(ft)
-    nvim_treesitter.install({ lang }):await(function(err)
-      if err then
-        vim.notify("Treesitter install error for ft: " .. ft .. " err: " .. err)
-        return
-      end
-
-      pcall(vim.treesitter.start, event.buf)
-      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-    end)
-  end,
-})
-
 -- A fast document parser that provides syntax coloring, folding, indentation, etc.
 -- https://github.com/nvim-treesitter/nvim-treesitter
 local treesitter = {
@@ -54,6 +23,9 @@ local ts_modules = {
   "MeanderingProgrammer/treesitter-modules.nvim",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
+  },
+  after = {
+    "nvim-treesitter"
   },
   opts = {
     ensure_installed = {
